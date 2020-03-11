da31790685033d606d2b72d59436c5576084aa50
e5chao <e5chao@ucsd.edu>
2020-03-10 21:23:18 -0700

    Adding features
    1. Add listener for when a tab becomes active
    2. Add bootstrap to start making extension look a bit better
    3. Refactor out close tab logic to accommodate for generic tab ages

--- js/background.js
+++ js/background.js
@@ -3,3 +3,7 @@ var tabsToCreationTimeMap = new Map();
 chrome.tabs.onCreated.addListener(function (tab) {
     tabsToCreationTimeMap[tab.id] = Date.now();
 });
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    tabsToCreationTimeMap[activeInfo.tabId] = Date.now();
+});
--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,22 @@
 var background = chrome.extension.getBackgroundPage();
 
+function closeTabs(tabAgeLimit, tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        if (background.tabsToCreationTimeMap[tabs[i].id] < tabAgeLimit) {
+            background.tabsToCreationTimeMap.delete(tabs[i].id);
+            chrome.tabs.remove(tabs[i].id);
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     var oneDayButton = document.getElementById('oneDay');
     oneDayButton.addEventListener('click', function () {
-
         chrome.tabs.query({}, function (tabs) {
             var currentTime = new Date(Date.now());
             var oneDayAgo = new Date(currentTime);
             oneDayAgo.setHours(currentTime.getHours() - 24);
-            for (var i = 0; i < tabs.length; i++) {
-                if (background.tabsToCreationTimeMap[tabs[i].id] < oneDayAgo) {
-                    background.tabsToCreationTimeMap.delete(tabs[i].id);
-                    chrome.tabs.remove(tabs[i].id);
-                }
-            }
+            closeTabs(oneDayAgo, tabs);
         });
     }, false);
 }, false);
