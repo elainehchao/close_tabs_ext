14363426db38bfb73ce3f3528c1171b45628749a
e5chao <e5chao@ucsd.edu>
2020-03-15 13:15:20 -0700

    Require the time value field to have a value before submitting

--- js/popup.js
+++ js/popup.js
@@ -38,6 +38,31 @@ function setStatus(color, text) {
     statusSpan.style.color = color;
 }
 
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+
+        // Get all form-groups in need of validation
+        var validateGroup = document.getElementsByClassName('validate-me');
+
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                for (var i = 0; i < validateGroup.length; i++) {
+                    validateGroup[i].classList.add('was-validated');
+                }
+            }, false);
+        });
+    }, false);
+})();
+
 document.addEventListener('DOMContentLoaded', function () {
     var tabAwayButton = document.getElementById('tabAwayBtn');
 
@@ -57,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 closeTabs(limit, tabs);
             });
         } else {
-            setStatus("red", "Invalid time value");
+            //            setStatus("red", "Invalid time value");
         }
     }, false);
 }, false);
